ab72336a8bfb2a010c6b5530c59d87b91b891470
Obinna Aliogor <118618713+obinnaaliogor@users.noreply.github.com>
2023-04-12 04:21:41 +0200

    Delete httpd.sh

--- remote-data-source/httpd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash  
- sudo useradd simon  
- sudo yum install httpd -y 
- sudo systemctl start httpd
- sudo systemctl enable httpd 
- sudo echo "<h1>Welcome To Engr Aliogo's Terraform series </h1>" | sudo tee -a /var/www/html/index.html
